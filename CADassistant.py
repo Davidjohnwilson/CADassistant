02aaf5a133b79a71addb4a6f9b3e2c6cf61cf2db
David Wilson <mrdavidjwilson@gmail.com>
2013-11-03 09:18:19 +0000

    Added init to select what mode to work in.

--- CADassistant.py
+++ CADassistant.py
@@ -1,1 +1,20 @@
 #CAD Assistant Program
+
+# Have access to the command line arguments
+import sys
+
+#Store them in arglist
+arglist = sys.argv[1:]
+
+if __name__ == "__main__":
+    if len(arglist)>0:
+        if arglist[0]=='interactive':
+            print "Interactive mode enabled\n"
+    else:
+        print "Manual mode enabled\n"
+
+
+def choosevariable(problem,variables):
+    if len(variables) == 1:
+        return variables
+
